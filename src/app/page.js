a2cda2b4622dbdbb21605d7c93d4196b4b9feeef
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2024-03-12 15:11:39 +0600

    user added

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
+import User from "@/components/User";
+
 export default function Home() {
   return (
     <div>
       <div className="flex justify-center items-center gap-10 my-10">
-        <h1 className="text-6xl font-bold text-center  text-red-600">
-          JOLOJOG
-        </h1>
+        <div>
+          <h1 className="text-6xl font-bold text-center  text-red-600 font-mono">
+            JOLooJOG
+          </h1>
+          <p className="text-right text-indigo-500 font-bold">MEAL SYSTEM</p>
+        </div>
 
         <div className="w-48 p-5 rounded-md bg-gradient-to-tl to-indigo-500 from-rose-600 text-white flex flex-col justify-center items-center gap-5">
           <h2 className="text-4xl font-bold">5000 /-</h2>
           <p className="font-semibold">CASH AVAILABLE</p>
         </div>
+      </div>
+
+      <div className="w-full m-5">
+        <User />
       </div>
     </div>
   );
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,118 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import srn from "@/../public/srn.jpg";
+import polok from "@/../public/polok.jpg";
+import arnob from "@/../public/arnob.jpg";
+import szn from "@/../public/szn.jpg";
+import soron from "@/../public/soron.jpg";
 
 const User = () => {
-  return <div></div>;
+  return (
+    <div className="bg-indigo-50 w-1/2 rounded-md shadow-lg p-5 flex flex-col gap-5">
+      {/* soron */}
+      <section className="flex justify-between items-center">
+        <div className="flex items-center gap-5">
+          <Image
+            src={srn}
+            alt="user"
+            width={200}
+            height={200}
+            className="size-14 rounded-full border"
+          />
+          <h1 className="font-semibold text-xl text-gray-500">
+            Eftekhar Jahan
+          </h1>
+          <input
+            type="search"
+            className="outline-none p-2 rounded-md"
+            placeholder="Enter Cash Digit.."
+          />
+        </div>
+        <span className="text-xl font-semibold text-gray-600">2000 BDT</span>
+      </section>
+
+      {/* polok */}
+      <section className="flex justify-between items-center">
+        <div className="flex items-center gap-5">
+          <Image
+            src={polok}
+            alt="user"
+            width={200}
+            height={200}
+            className="size-14 rounded-full border"
+          />
+          <h1 className="font-semibold text-xl text-gray-500">Samir Islam</h1>
+          <input
+            type="search"
+            className="outline-none p-2 rounded-md"
+            placeholder="Enter Cash Digit.."
+          />
+        </div>
+        <span className="text-xl font-semibold text-gray-600">2000 BDT</span>
+      </section>
+
+      {/* Arnob */}
+      <section className="flex justify-between items-center">
+        <div className="flex items-center gap-5">
+          <Image
+            src={arnob}
+            alt="user"
+            width={200}
+            height={200}
+            className="size-14 rounded-full border"
+          />
+          <h1 className="font-semibold text-xl text-gray-500">Arnob Das</h1>
+          <input
+            type="search"
+            className="outline-none p-2 rounded-md"
+            placeholder="Enter Cash Digit.."
+          />
+        </div>
+        <span className="text-xl font-semibold text-gray-600">2000 BDT</span>
+      </section>
+
+      {/* Sezan */}
+      <section className="flex justify-between items-center">
+        <div className="flex items-center gap-5">
+          <Image
+            src={szn}
+            alt="user"
+            width={200}
+            height={200}
+            className="size-14 rounded-full border"
+          />
+          <h1 className="font-semibold text-xl text-gray-500">Simur Ahmed</h1>
+          <input
+            type="search"
+            className="outline-none p-2 rounded-md"
+            placeholder="Enter Cash Digit.."
+          />
+        </div>
+        <span className="text-xl font-semibold text-gray-600">2000 BDT</span>
+      </section>
+
+      {/* soron2 */}
+      <section className="flex justify-between items-center">
+        <div className="flex items-center gap-5">
+          <Image
+            src={soron}
+            alt="user"
+            width={200}
+            height={200}
+            className="size-14 rounded-full border"
+          />
+          <h1 className="font-semibold text-xl text-gray-500">Sharon Paul</h1>
+          <input
+            type="search"
+            className="outline-none p-2 rounded-md"
+            placeholder="Enter Cash Digit.."
+          />
+        </div>
+        <span className="text-xl font-semibold text-gray-600">2000 BDT</span>
+      </section>
+    </div>
+  );
 };
 
 export default User;
